35eaa0d8b22bc276918aae8e8ff16732e3af706e
lws803 <lws803@gmail.com>
2023-03-10 19:24:55 -0500

    added sentry error monitoring

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-module.exports = nextConfig
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const moduleExports = {
+  // Your existing module.exports
+  reactStrictMode: true,
+};
+
+const sentryWebpackPluginOptions = {
+  silent: true,
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
